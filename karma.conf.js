637a3c6e4a0c749af272223757f9f022f001c502
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-11-03 22:53:39 -0300

    fix: change name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/viagens'),
+      dir: require('path').join(__dirname, './coverage/NotasProf'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'viagens'`, () => {
+  it(`should have as title 'NotasProf'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('viagens');
+    expect(app.title).toEqual('NotasProf');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('viagens app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('NotasProf app is running!');
   });
 });
