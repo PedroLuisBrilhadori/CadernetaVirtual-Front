4d9eba390e5ebc1f35facaabbd56b425706fa85a
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-11-05 09:19:21 -0300

    feat(toolbar): adiciona input de titulo

--- libs/ui/src/components/tool-bar/toolbar.component.ts
+++ libs/ui/src/components/tool-bar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 
 @Component({
@@ -11,6 +11,8 @@ import { Component, OnInit } from "@angular/core";
 
 export class ToolbarComponent {
 
+  @Input('titleToolbar')
+  title: string;
 
 }
 
