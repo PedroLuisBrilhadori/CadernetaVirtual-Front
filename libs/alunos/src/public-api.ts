60d4e31d3219f506474de92ef925f0c7056cea79
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-05 21:22:15 -0300

    remove bibliotecas próprias

--- libs/alunos/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of alunos
- */
-
-export * from './lib/alunos.service';
-export * from './lib/alunos.component';
-export * from './lib/alunos.module';
--- libs/ui/public-api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of ui
- */
-
-export * from './lib';
