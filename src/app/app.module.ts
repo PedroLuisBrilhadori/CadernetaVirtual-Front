28113146d8ddbef51cb5537db86b10b558f8073e
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-06 13:45:20 -0300

    Revert "test: testando prettier"
    This reverts commit ad00f5b8a296165db6d634e3248f4aeeee69160a.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TableModule } from './table/table.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, TableModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/table/table.module.ts
+++ src/app/table/table.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table.component';
-import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [TableComponent],
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule],
   exports: [TableComponent],
 })
 export class TableModule {}
