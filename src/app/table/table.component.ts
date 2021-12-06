7c9f34315f7589df428508579ee7f3580114e658
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-06 13:24:11 -0300

    fix: altera forma do husky usar o prettier

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ColumnMode } from '@swimlane/ngx-datatable';
-
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
