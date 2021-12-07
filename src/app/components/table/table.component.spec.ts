51bd2c108050379050de37f808ab464e68a59efb
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-12-07 12:37:44 -0300

    test: inicia arquivos de teste

--- src/app/components/table/table.component.spec.ts
+++ src/app/components/table/table.component.spec.ts
@@ -1,10 +1,47 @@
+import { Component, NgModule, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ColumnsTableModel, TableModule } from '.';
 
 import { TableComponent } from './table.component';
 
+@Component({
+  template: `
+    <app-table
+      [dataSource]="data"
+      [displayedColumns]="columns"
+      (selectedRow)="selected($event)"
+      [multiSelect]="true"
+    ></app-table>
+  `,
+})
+class TemplateTestTable {
+  @ViewChild(TableComponent)
+  table: TableComponent;
+
+  data: any[] = [
+    { id: '1', teste: 'Pedro' },
+    { id: '2', teste: 'Paulo' },
+  ];
+
+  columns: ColumnsTableModel[] = [
+    { name: 'id', displayedName: 'id', sort: false },
+    { name: 'teste', displayedName: 'nome', sort: true },
+  ];
+
+  selected(event: any) {
+    console.log(event);
+  }
+}
+
+@NgModule({
+  declarations: [TemplateTestTable],
+  imports: [TableModule],
+})
+class TestModule {}
+
 describe('TableComponent', () => {
-  let component: TableComponent;
-  let fixture: ComponentFixture<TableComponent>;
+  let component: TemplateTestTable;
+  let fixture: ComponentFixture<TemplateTestTable>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,7 +50,7 @@ describe('TableComponent', () => {
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TableComponent);
+    fixture = TestBed.createComponent(TemplateTestTable);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
